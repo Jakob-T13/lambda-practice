7e0ca36930548b84eb88018a402812746342ce19
Jakob Thunen <jthunen@dev-10.com>
2022-04-11 14:53:56 -0500

    added info per neighborhood

--- lambdapractice.py
+++ lambdapractice.py
@@ -18,13 +18,27 @@ tot_time = reduce(lambda t1, t2: t1 + float(t2["totaltime"]), filtered_lst, 0)
 avg_tot_time = tot_time / len(filtered_lst)
 print(f"Average total time: {avg_tot_time}")
 
+# filtered_lst_trunc = filtered_lst[:5000]
+
 neighborhood_lst = []
 for i in filtered_lst:
     if i["neighborhood"] not in neighborhood_lst:
-        neighborhood_lst += i["neighborhood"]
-
-neighborhood_lst = neighborhood_lst.sort()
+        neighborhood_lst.append(i["neighborhood"])
+        
+neighborhood_lst.sort()
 
 for i in neighborhood_lst:
     print(f"{i}:")
+    neighborhood_filter = list(filter(lambda row: row["neighborhood"] == i, filtered_lst))
     
+    tot_response_time = reduce(lambda t1, t2: t1 + float(t2["totalresponsetime"]), neighborhood_filter, 0)
+    avg_tot_response_time = tot_response_time / len(neighborhood_filter)
+    print(f"\tAverage total response time: {avg_tot_response_time}")
+
+    tot_dispatch_time = reduce(lambda t1, t2: t1 + float(t2["dispatchtime"]), neighborhood_filter, 0)
+    avg_dispatch_time = tot_dispatch_time / len(neighborhood_filter)
+    print(f"\tAverage dispatch time: {avg_dispatch_time}")
+
+    tot_time = reduce(lambda t1, t2: t1 + float(t2["totaltime"]), neighborhood_filter, 0)
+    avg_tot_time = tot_time / len(neighborhood_filter)
+    print(f"\tAverage total time: {avg_tot_time}")
