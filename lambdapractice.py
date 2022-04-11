88db129a574b49f758c8cf7702efc7509eba6dd1
Jakob Thunen <jthunen@dev-10.com>
2022-04-11 15:12:21 -0500

    Output info per neighberhood to json instead of console

--- lambdapractice.py
+++ lambdapractice.py
@@ -1,5 +1,6 @@
 import csv
 from functools import reduce
+import json
 
 csvf = open("911_Calls_for_Service_(Last_30_Days).csv")
 reader = csv.DictReader(csvf)
@@ -18,7 +19,6 @@ tot_time = reduce(lambda t1, t2: t1 + float(t2["totaltime"]), filtered_lst, 0)
 avg_tot_time = tot_time / len(filtered_lst)
 print(f"Average total time: {avg_tot_time}")
 
-# filtered_lst_trunc = filtered_lst[:5000]
 
 neighborhood_lst = []
 for i in filtered_lst:
@@ -27,18 +27,34 @@ for i in filtered_lst:
         
 neighborhood_lst.sort()
 
+output_lst = []
+
+print("Generating json...")
+
 for i in neighborhood_lst:
-    print(f"{i}:")
+    # print(f"{i}:")
     neighborhood_filter = list(filter(lambda row: row["neighborhood"] == i, filtered_lst))
     
     tot_response_time = reduce(lambda t1, t2: t1 + float(t2["totalresponsetime"]), neighborhood_filter, 0)
     avg_tot_response_time = tot_response_time / len(neighborhood_filter)
-    print(f"\tAverage total response time: {avg_tot_response_time}")
+    # print(f"\tAverage total response time: {avg_tot_response_time}")
 
     tot_dispatch_time = reduce(lambda t1, t2: t1 + float(t2["dispatchtime"]), neighborhood_filter, 0)
     avg_dispatch_time = tot_dispatch_time / len(neighborhood_filter)
-    print(f"\tAverage dispatch time: {avg_dispatch_time}")
+    # print(f"\tAverage dispatch time: {avg_dispatch_time}")
 
     tot_time = reduce(lambda t1, t2: t1 + float(t2["totaltime"]), neighborhood_filter, 0)
     avg_tot_time = tot_time / len(neighborhood_filter)
-    print(f"\tAverage total time: {avg_tot_time}")
+    # print(f"\tAverage total time: {avg_tot_time}")
+    output_dict = {
+        "neighborhood" : i,
+        "avg_total_response_time" : avg_tot_response_time,
+        "avg_dispatch_time" : avg_dispatch_time,
+        "avg_total_time" : avg_tot_time
+    }
+    out_json = json.dumps(output_dict)
+    output_lst.append(out_json)
+    
+outf = open("output.json", "wt")
+outf.write(str(output_lst))
+outf.close()
